ec036dc95b8186104df37cb9f6430c7ae0666d6f
Younis Bamoman <younisobed82@gmail.com>
2021-06-22 11:39:55 +0300

    add website

--- academy/controllers/controllers.py
+++ academy/controllers/controllers.py
@@ -1,17 +1,41 @@
-# -*- coding: utf-8 -*-
 from odoo import http
+# from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.http import request
+# from datetime import datetime
 
-class Academy(http.Controller):
+class CardCreateRequest(http.Controller):
+  @http.route('/academyss/new', auth='user', type='http', website=True)
+  def new(self, **kw):
+    return request.render('academy.academy_template')
 
-     @http.route('/academy/academy/', auth='public', website=True)
-     def index(self, **kw):
-         Teachers = http.request.env['academy.teachers']
-         return http.request.render('academy.index', {
-             'teachers': Teachers.search([])
-         })
+  @http.route('/create_cards/create', auth='user', type='http', csrf=False, website=True)
+  def create(self, **kw):
+ 
+      request.env['academy.teachers'].sudo().create(kw)
+      return request.render('academy.patient_thanks', )
+
+
+
+
+
+
+# from odoo import http
+# from odoo.http import request
+# class Academy(http.Controller):
+
+#      @http.route('/academyss/new', type='http', auth="user", website=True)
+#      def new(self, **kw):
+#       return request.render('academy.academy_template',{})
          
-     @http.route('/academy/<model("academy.teachers"):teacher>/', auth='public', website=True)
-     def teacher(self, teacher):
-      return http.request.render('academy.biography', {
-        'person': teacher
-       })
+#      @http.route('/create_cards/create',type='http', auth="user", website=True)
+#      def create(self, **kw):
+#         print(kw)
+      #    request_values = {
+      #    'note': kw.get('note'),
+      #    'email': kw.get('email'),
+      #    'mobile': kw.get('mobile'),
+      #    'name': kw.get('name')
+      #      }
+      #    request.env['card_create_request'].sudo().create(request_values)
+      #    request.env['academy.teachers'].sudo().create(kw)
+      #    return request.render('academy.patient_thanks', )
---
+++ care_cards/controllers/card_request.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+
+class CreateRequest(http.Controller):
+    @http.route('/cards_requests/new' , auth='user', type='http', website=True)
+    def new(self, **kw):
+        return request.render('care_cards.template_web')
+    
+    @http.route('/create_cards/create', auth='user', type='http', csrf=False, website=True)
+    def create(self, **kw):
+ 
+      request.env['card_request'].sudo().create(kw)
+      return request.render('care_cards.patient_thanks')
